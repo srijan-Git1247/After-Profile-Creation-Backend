be4c8928e8cec69cd942277271d788130af6d726
srijan <bagdassrijan@hotmail.com>
2022-03-16 00:01:46 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9e57c8e37b292ad8ee72ef2388ea56b8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0f7434b4-e8aa-49c5-9912-9b60f8eae398'
+};
